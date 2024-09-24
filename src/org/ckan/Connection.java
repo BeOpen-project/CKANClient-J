e07b17f9fea2fd1c05bf53f3af58217194471661
Marco Sajeva <57068725+Mingo07-dev@users.noreply.github.com>
2024-09-24 11:27:36 +0200

    Update Connection.java

--- src/org/ckan/Connection.java
+++ src/org/ckan/Connection.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpHost;
@@ -164,7 +166,7 @@ public final class Connection {
 			}
 			proxy = new HttpHost(getProperty("https.proxyHost"), port, "https");
 			
-			DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
+						DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
 			httpclient = HttpClients.custom()
 			                    .setRoutePlanner(routePlanner)
 			                    .setSSLSocketFactory(
